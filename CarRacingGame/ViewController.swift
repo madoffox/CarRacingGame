3c63ecb67bd218d30c8811fc6d79acf67739a64c
vladislav <vlad.shtytcko@lookout.com>
2021-09-04 17:49:25 +0300

    Initial Commit

---
+++ CarRacingGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarRacingGame
+//
+//  Created by Vladislav on 4.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
